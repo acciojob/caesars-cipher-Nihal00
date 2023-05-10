9d788316637ed695cc04d5bd2389632f68a48630
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-10 21:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,8 +13,12 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	let array = encodedStr.split("");
+	for(let i = 0; i < array.length; i++){
+		decodedArr[i] = lookup.array[i];
+	}
 
-  return ;//return decodedArr
+  return decodedArr;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
