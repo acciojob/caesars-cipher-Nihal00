3cad522a32911333d6ef9ce28b0a577b424e8d36
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-11 14:16:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,12 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-	let array = encodedStr.split("");
-	for(let i = 0; i < array.length; i++){
-		decodedArr[i] = lookup.array[i];
+	for(let i=0; i<encodedStr.length; i++){
+		if(lookup.encodedStr[i] === undefined){
+			decodedArr[i] = encodedStr[i];
+		} else {
+			decodedArr[i] = lookup.encodedStr[i];
+		}
 	}
 
   return decodedArr;//return decodedArr
@@ -23,6 +26,6 @@ function rot13(encodedStr){
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(rot13("SERR YBIR? NPPVBWBO"));
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 module.exports = rot13;
